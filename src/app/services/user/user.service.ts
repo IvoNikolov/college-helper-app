ff6349afeb1620e76949b1ce142621faeea8a60b
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-12-21 00:18:51 +0200

    Add users

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  private _users = new BehaviorSubject<User[]>([
+    new User('U1', 'Dave', '819497737'),
+    new User('U2', 'Tim', '819497937'),
+    new User('U2', 'Jack', '707692537')
+  ]);
+
+  get events() {
+      return this._users.asObservable();
+
+  }
+
 
   constructor() { }
 }
