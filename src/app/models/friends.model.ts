e733c5ba816b656cc1d0549ff46ddeb8df54f56c
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-12-23 23:54:13 +0200

    Add friends model

---
+++ src/app/models/friends.model.ts
@@ -0,0 +1,13 @@
+export class Friends {
+    public id: string;
+    public name: string;
+    public day: string;
+    public month: string;
+
+    constructor(id: string, name: string, day: string, month: string) {
+        this.id = id;
+        this.name = name;
+        this.day = day;
+        this.month = month;
+    }
+}
