cc87c3edb12260155cc4c09f36978c5a2f65185b
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-12-24 22:54:20 +0200

    Update friends model

--- src/app/models/friends.model.ts
+++ src/app/models/friends.model.ts
@@ -1,12 +1,14 @@
 export class Friends {
     public id: string;
     public name: string;
+    public familyName: string;
     public day: string;
     public month: string;
 
-    constructor(id: string, name: string, day: string, month: string) {
+    constructor(id: string, name: string, familyName: string, day: string, month: string) {
         this.id = id;
         this.name = name;
+        this.familyName = familyName;
         this.day = day;
         this.month = month;
     }
