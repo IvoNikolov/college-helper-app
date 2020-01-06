db952be3366f75b2bf71a0c625a3e807cc7760b4
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-01-06 22:03:03 +0200

    Add place

--- src/app/models/place.model.ts
+++ src/app/models/place.model.ts
@@ -1,3 +1,8 @@
 export class Place {
-    constructor() {}
+    public id: number;
+    public name: string;
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
 }
