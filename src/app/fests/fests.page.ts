ecb0a22310b46a042f892ae688e25a8c0ebc2bbe
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-01-08 22:49:31 +0200

    Set fests

--- src/app/fests/fests.page.ts
+++ src/app/fests/fests.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Fest } from '../models/fest.model';
 
 @Component({
   selector: 'app-fests',
@@ -7,12 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FestsPage implements OnInit {
 
-  fests = [];
+  fests: Fest[];
   currentDate = new Date();
 
   constructor() { }
 
   ngOnInit() {
+
     console.log(this.currentDate.getDate(), this.currentDate.getDay(), this.currentDate.getMonth());
   }
 
