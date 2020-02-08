e57998057d0fc9b8ae171848dd8445b851009ee7
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-02-08 12:22:45 +0200

    Add more friends

--- src/app/friends/friends.page.ts
+++ src/app/friends/friends.page.ts
@@ -3,18 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.page.html',
-  styleUrls: ['./friends.page.scss'],
+  styleUrls: ['./friends.page.scss']
 })
 export class FriendsPage implements OnInit {
+  friendsArray = [
+    {
+      name: 'Daniel',
+      birthdayDay: '12',
+      birthdayMonth: '5'
+    },
+    {
+      name: 'Susan',
+      birthdayDay: '2',
+      birthdayMonth: '3'
+    }
+  ];
+  constructor() {}
 
-  friendsArray = [{
-    name: 'Daniel',
-    birthdayDay: '',
-    birthdayMonth: '',
-  }];
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
