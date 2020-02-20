b744a7a255c672838d3a3a7d170351cdc03473c3
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-02-20 23:45:51 +0200

    Add expiration date

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,8 +1,10 @@
 export class Product {
     public id: number;
     public name: string;
-    constructor(id: number, name: string) {
+    public expirationDate: string;
+    constructor(id: number, name: string, expirationDate: string) {
         this.id = id;
         this.name = name;
+        this.expirationDate = expirationDate;
     }
 }
