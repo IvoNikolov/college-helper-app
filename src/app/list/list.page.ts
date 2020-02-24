824977b2663c2e1196d70fe4ef3935089074705d
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-02-24 13:27:42 +0200

    Add product

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -8,7 +8,7 @@ import { Product } from '../models/product.model';
 })
 export class ListPage implements OnInit {
 
-  products: Product[] = [new Product(1, 'Milk', new Date('2020-03-16') )];
+  products: Product[] = [new Product(1, 'Milk', new Date('2020-03-16')), new Product(2, 'Mustard', new Date('2020-06-26'))];
 
   constructor() {
 
